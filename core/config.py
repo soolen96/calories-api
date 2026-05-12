7bbeaedda810e1aacb89c80798831761427a7061
soolen96 <solalguero@gmail.com>
2026-05-11 19:58:56 -0500

    Implement basic info endpoint and data handling for person profiles

---
+++ core/config.py
@@ -0,0 +1,26 @@
+import os
+import json
+from models.person import Person
+
+
+def save_info(person_info:Person):
+    path = "data/basic-info-people.json"
+    new_user = person_info.model_dump()
+
+    users_list = []
+
+    if os.path.exists(path):
+        with open(path, "r") as file:
+            try: 
+                users_list = json.load(file)
+            except json.JSONDecodeError:
+                users_list = []
+
+    users_list.append(new_user)
+
+    with open(path,"w") as file:
+        json.dump(users_list, file, indent=4)
+
+    return {
+        "mensaje":f"perfil creado y guardado exitosamente en JSON exitosamente de: :{person_info.name}"
+    }
